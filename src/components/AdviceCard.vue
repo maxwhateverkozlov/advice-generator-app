6506ecb5add05913eda8cacaad4cace8c97d08a3
Maksym Kozlov <maksimpro1337@gmail.com>
2026-03-18 21:51:18 +0100

    style: styled components

---
+++ src/components/AdviceCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+import DiceButton from './DiceButton.vue';
+
+defineProps({
+  id: Number,
+  text: String
+});
+
+</script>
+
+<template>
+  <div class="bg-dark-grayish-blue rounded-2xl p-8 max-w-md w-full text-center shadow-2xl relative">
+      <p class="text-neon-green font-extralight mb-8 text-[11px] uppercase ">Advice #{{ id }}</p>
+      <blockquote class="text-light-cyan">"{{ text }}"</blockquote>
+      <div class="my-8">
+        <img src="/pattern-divider-mobile.svg" 
+      alt="divider"
+      class="block md:hidden w-full">
+      <img src="/pattern-divider-desktop.svg" 
+      alt="divider"
+      class="hidden md:block w-full">
+      </div>
+        <DiceButton @click="$emit('refresh')" />
+    </div>
+</template>
---
+++ src/components/DiceButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+const emit = defineEmits(['click'])
+</script>
+
+<template>
+  <button @click="emit('click')" class="absolute -bottom-8 left-1/2 -translate-x-1/2 bg-neon-green p-5 rounded-full hover:shadow-[0_0_40px_rgba(82,255,168,0.8)] transition-all duration-300 group cursor-pointer">
+        <img src="/icon-dice.svg" alt="dice" class="w-6 h-6 group-hover:rotate-180 transition-transform duration-500">
+      </button>
+</template>
