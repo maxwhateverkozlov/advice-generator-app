ff35d2422430b16c97b89733f5510fa941520b0e
Maksym Kozlov <maksimpro1337@gmail.com>
2026-03-18 21:07:51 +0100

    feat: connected tailwind and added own styles

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,15 @@
 </script>
 
 <template>
-  
+  <main class="bg-dark-blue min-h-screen flex justify-center items-center">
+    <div class="bg-dark-grayish-blue rounded-2xl p-8 max-w-md w-full text-center shadow-2xl relative">
+      <h1 class="text-neon-green">Advice #<div id="span">0</div></h1>
+      <p>“It's not about being perfect, it's about being in the moment.”</p>
+      <img src="" alt="divider">
+      <img src="" alt="divider">
+      <button>
+        <img src="" alt="dice">
+      </button>
+    </div>
+  </main>
 </template>
